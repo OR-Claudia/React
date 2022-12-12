4cbac87d07244d5acad887b071f710254b8a11e8
Claudia N <claudia.nicolae@oracle.com>
2022-12-12 14:50:42 +0200

    Pushing changes

--- DeveloperCourse/ec-app/src/App.js
+++ DeveloperCourse/ec-app/src/App.js
@@ -1,5 +1,39 @@
 const App = () => {
-  return <div>Hello World</div>;
+	const categories = [
+		{
+			id: 1,
+			title: "Hats",
+		},
+		{
+			id: 2,
+			title: "Jackets",
+		},
+		{
+			id: 3,
+			title: "Sneakers",
+		},
+		{
+			id: 4,
+			title: "Women's",
+		},
+		{
+			id: 5,
+			title: "Men's",
+		},
+	];
+	return (
+		<div className="categories-container">
+			{categories.map(({ title, id }) => (
+				<div className="category-container" key={id}>
+					<div className=""></div>
+					<div className="category-body-container">
+						<h2>{title}</h2>
+						<p>Shop now</p>
+					</div>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default App;
